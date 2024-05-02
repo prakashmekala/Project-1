5a3ec391957799c20bc32a61111d293ab9dec382
Prakash Mekala <prakash.mekala@gmail.com>
2024-05-02 01:52:15 +0000

    Data retrival

--- webmodule/webapp/controller/Landing.controller.js
+++ webmodule/webapp/controller/Landing.controller.js
@@ -9,7 +9,39 @@ sap.ui.define([
 
         return Controller.extend("webmodule.controller.Landing", {
             onInit: function () {
+                let URL = "/v2/catalog/Products";
+                $.ajax({
+                    url: URL,
+                    type: "GET",
+                    success: function (oData,response) {
+                        console.log("got the data")
+    				},
+                    error: function (error) {
+                        sap.m.MessageBox.error(JSON.parse(error.responseText).message, {
+                            actions: ["OK"],
+                            onClose: function (sAction) { }
+                        });
+                    }
+                });
+                let that = this;
+                let oDataModel = this.getOwnerComponent().getModel();
+                let sModel=new sap.ui.model.json.JSONModel();
+		        oDataModel.read("/Products", {
+                    success: function(oData, response){
+                        sModel.setData(oData.results);
+                        that.getView().setModel(sModel,"Products");
+                        // that.getView().getModel("StatementList").refresh();
 
+                        // let savedVar = JSON.parse(localStorage.getItem("filterVariant"));
+                        // if(savedVar.length>0){
+                        //     let oBinding = this.getView().byId("idTable").getBinding("items");
+                        //     oBinding.filter(savedVar, sap.ui.model.FilterType.Application).sOperationMode = "Client";
+                        // } 
+                    },
+                    errors: function(error){
+                        console.log(error);
+                    }
+                })
             }
         });
     });
